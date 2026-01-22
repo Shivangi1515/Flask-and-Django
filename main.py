64a2147ba3983dbe5ef5a1304adb5ec1b9b8361a
Shivangi <shivangichaurasia1515@gmail.com>
2026-01-22 11:22:52 +0530

    Added Flask app and html template

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("university.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
